a343f374112cbed21f701ec8b55d9ee7d3502eb0
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-07-17 14:22:37 -0400

    muros verticales listos

--- project/main.js
+++ project/main.js
@@ -53,13 +53,13 @@ function init() {
   };
 
   meshFloor = new THREE.Mesh(
-    new THREE.PlaneGeometry(170, 110, 10, 10),
+    new THREE.PlaneGeometry(160, 100, 10, 10),
     new THREE.MeshPhongMaterial({ color: 0xffffff })
   );
   meshFloor.rotation.x -= Math.PI / 2;
   meshFloor.receiveShadow = true;
   scene.add(meshFloor);
-  meshFloor.position.set(0, 0, 0);
+  //meshFloor.position.set(0, 0, 0);
 
   ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
   scene.add(ambientLight);
@@ -76,38 +76,37 @@ function init() {
   wallBumpMap = textureLoader.load('images/wall_bump.jpg');
   wallNormalMap = textureLoader.load('images/wall_normal.jpg');
 
-//  bloques de henry 
-var textureLoader = new THREE.TextureLoader();
-crateTexture1 = new textureLoader.load('images/wall_difuse.jpg');   
-crateBumpMap1 = new textureLoader.load('images/wall_bump.jpg');  
-  
-crate1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 10, 110),
-  new THREE.MeshPhongMaterial({
-    color: 0xffffff,
-    map: crateTexture1,
-    bumpMap: crateBumpMap1
-  })
-);  
-  scene.add(crate1);
-  crate1.position.set(-85, 3, 0);
+  //  bloques de henry
+  var textureLoader = new THREE.TextureLoader();
+  crateTexture1 = new textureLoader.load('images/wall_difuse.jpg');
+  crateBumpMap1 = new textureLoader.load('images/wall_bump.jpg');
 
-  crate2 = new THREE.Mesh(
-    new THREE.BoxGeometry(170, 10, 1),
+  crate1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 20, 100),
     new THREE.MeshPhongMaterial({
       color: 0xffffff,
       map: crateTexture1,
-      bumpMap: crateBumpMap1
+      bumpMap: crateBumpMap1,
     })
-  );  
-    scene.add(crate2);
-    crate2.position.set(0, 5, -55);
+  );
+  scene.add(crate1);
+  crate1.position.set(-80, 10, 0);
 
-// fin bloques de henry
+  crate2 = new THREE.Mesh(
+    new THREE.BoxGeometry(160, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: crateTexture1,
+      bumpMap: crateBumpMap1,
+    })
+  );
+  scene.add(crate2);
+  crate2.position.set(0, 10, -50);
 
+  // fin bloques de henry
 
   wall = new THREE.Mesh(
-    new THREE.BoxGeometry(170, 20, 1),
+    new THREE.BoxGeometry(160, 20, 1),
     new THREE.MeshPhongMaterial({
       color: 0xffffff,
       map: wallTexture,
@@ -115,8 +114,161 @@ crate1 = new THREE.Mesh(
       normalMap: wallNormalMap,
     })
   );
-  wallx = new THREE.Mesh(
-    new THREE.BoxGeometry(110, 20, 1),
+  pared1 = new THREE.Mesh(
+    new THREE.BoxGeometry(100, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared2 = new THREE.Mesh(
+    new THREE.BoxGeometry(80, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared3 = new THREE.Mesh(
+    new THREE.BoxGeometry(30, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared4 = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared5 = new THREE.Mesh(
+    new THREE.BoxGeometry(10, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared6 = new THREE.Mesh(
+    new THREE.BoxGeometry(90, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared7 = new THREE.Mesh(
+    new THREE.BoxGeometry(80, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared8 = new THREE.Mesh(
+    new THREE.BoxGeometry(70, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared9 = new THREE.Mesh(
+    new THREE.BoxGeometry(60, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared10 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared11 = new THREE.Mesh(
+    new THREE.BoxGeometry(40, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared12 = new THREE.Mesh(
+    new THREE.BoxGeometry(30, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared13 = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared14 = new THREE.Mesh(
+    new THREE.BoxGeometry(10, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared15 = new THREE.Mesh(
+    new THREE.BoxGeometry(20, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared16 = new THREE.Mesh(
+    new THREE.BoxGeometry(30, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared17 = new THREE.Mesh(
+    new THREE.BoxGeometry(40, 20, 1),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      map: wallTexture,
+      bumpMap: wallBumpMap,
+      normalMap: wallNormalMap,
+    })
+  );
+  pared18 = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 20, 1),
     new THREE.MeshPhongMaterial({
       color: 0xffffff,
       map: wallTexture,
@@ -126,14 +278,65 @@ crate1 = new THREE.Mesh(
   );
 
   scene.add(wall);
-  wall.position.set(0, 10, 55);
+  wall.position.set(0, 10, 50);
   wall.rotation.y -= Math.PI * 2;
   wall.receiveShadow = true;
   wall.castShadow = true;
+  scene.add(pared1);
+  pared1.position.set(80, 10, 0);
+  pared1.rotation.y += Math.PI / 2;
+  scene.add(pared2);
+  pared2.position.set(70, 10, 0);
+  pared2.rotation.y += Math.PI / 2;
+  scene.add(pared3);
+  pared3.position.set(60, 10, 35);
+  pared3.rotation.y += Math.PI / 2;
+  scene.add(pared4);
+  pared4.position.set(60, 10, 0);
+  pared4.rotation.y += Math.PI / 2;
+  scene.add(pared5);
+  pared5.position.set(60, 10, -25);
+  pared5.rotation.y += Math.PI / 2;
+  scene.add(pared6);
+  pared6.position.set(50, 10, 5);
+  pared6.rotation.y += Math.PI / 2;
+  scene.add(pared7);
+  pared7.position.set(40, 10, 10);
+  pared7.rotation.y += Math.PI / 2;
+  scene.add(pared8);
+  pared8.position.set(30, 10, 0);
+  pared8.rotation.y += Math.PI / 2;
+  scene.add(pared9);
+  pared9.position.set(20, 10, 0);
+  pared9.rotation.y += Math.PI / 2;
+  scene.add(pared10);
+  pared10.position.set(10, 10, 0);
+  pared10.rotation.y += Math.PI / 2;
+  scene.add(pared11);
+  pared11.position.set(0, 10, 0);
+  pared11.rotation.y += Math.PI / 2;
+  scene.add(pared12);
+  pared12.position.set(-10, 10, 0);
+  pared12.rotation.y += Math.PI / 2;
+  scene.add(pared13);
+  pared13.position.set(-20, 10, 0);
+  pared13.rotation.y += Math.PI / 2;
+  scene.add(pared14);
+  pared14.position.set(-30, 10, 0);
+  pared14.rotation.y += Math.PI / 2;
+  scene.add(pared15);
+  pared15.position.set(-40, 10, 0);
+  pared15.rotation.y += Math.PI / 2;
+  scene.add(pared16);
+  pared16.position.set(-50, 10, 0);
+  pared16.rotation.y += Math.PI / 2;
+  scene.add(pared17);
+  pared17.position.set(-60, 10, 0);
+  pared17.rotation.y += Math.PI / 2;
+  scene.add(pared18);
+  pared18.position.set(-70, 10, 0);
+  pared18.rotation.y += Math.PI / 2;
 
-  scene.add(wallx);
-  wallx.position.set(85, 10, 0);
-  wallx.rotation.y += Math.PI / 2;
   // Cargar modelos
 
   for (var _key in models) {
@@ -184,18 +387,18 @@ function onResourcesLoaded() {
   meshes.wall4 = models.wall.mesh.clone();
 
   meshes.wall1.position.set(-14, -1, 8);
-  scene.add(meshes.wall1);
+  // scene.add(meshes.wall1);
 
   meshes.wall2.position.set(-14, -1, -4);
-  scene.add(meshes.wall2);
+  // scene.add(meshes.wall2);
 
   meshes.wall3.position.set(-8, -1, 14);
   meshes.wall3.rotation.y += 1.55;
-  scene.add(meshes.wall3);
+  // scene.add(meshes.wall3);
 
   meshes.wall4.position.set(3.9, -1, 14.2);
   meshes.wall4.rotation.y += 1.56;
-  scene.add(meshes.wall4);
+  // scene.add(meshes.wall4);
   // arma
   meshes.playerweapon = models.uzi.mesh.clone();
   meshes.playerweapon.position.set(0, 2, 0);
